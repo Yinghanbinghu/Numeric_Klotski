30492c5b74efad7171c24b8df9eecda2eeb1fd86
admin <1135272289@qq.com>
2022-12-05 20:05:07 +0800

    Search是基础的搜索 搜索树优化完传上去

---
+++ src/TreeSearch.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+import edu.princeton.cs.algs4.SeparateChainingHashST;
+public class TreeSearch {
+    ArrayList<State> map;
+    int currentStep;
+}
--- src/test.java
+++ src/test.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 
 public class test {
     public static void main(String[] args) {
-        Search search=new Search(new int[][]{{0,0,0},{0,1,2},{0,0,3}},3);
+        Search search=new Search(new int[][]{{0,0,0,0,0,},{0,13,4,3,5},{0,15,2,8,10},{0,9,14,1,6},{0,12,11,7,0}},15);
         ArrayList<int[][]> a=search.doSearch();
         for (int[][] b:a
              ) {
