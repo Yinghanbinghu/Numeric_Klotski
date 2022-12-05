b4c2fa9ab34e6f508f199e8f6c0e529cf05b2b9d
admin <1135272289@qq.com>
2022-12-05 16:48:11 +0800

    等一下做优化

--- src/test.java
+++ src/test.java
@@ -1,2 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class test {
+    public static void main(String[] args) {
+        Search search=new Search(new int[][]{{0,0,0},{0,1,2},{0,0,3}},3);
+        ArrayList<int[][]> a=search.doSearch();
+        for (int[][] b:a
+             ) {
+            for (int [] c:b
+                 ) {
+                for (int d:c
+                     ) {
+                    System.out.print(d);
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
 }
